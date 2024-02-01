4a5dcb9dde1ef7fdf2b28964681386c6a15f0449
Felipe Chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2024-02-01 19:59:10 -0300

    add tower defense 32 project images

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import reactSvgPlugin from 'vite-plugin-react-svg';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import reactSvgPlugin from "vite-plugin-react-svg";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), reactSvgPlugin({ svgoConfig: {} })],
+  plugins: [react(), reactSvgPlugin({ svgoConfig: {} })],
 });
